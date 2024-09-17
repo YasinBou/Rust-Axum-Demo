9af552e835356fedc678a10b6334d111493f62bb
Yasin <yasin@Yasins-MacBook-Air.local>
2024-09-17 23:32:01 +0200

    Add hello world endpoint

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let hello_route = Router::new().route(
+        "/hello",
+        get(|| async {
+            Html("Hello World</strong>") 
+        }),
+    );
+
+    let address: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener: TcpListener = TcpListener::bind(&address).await.unwrap();
+    println!("->> LISTENING ON {address}\n");
+
+    axum::serve(listener, hello_route.into_make_service()).await.unwrap();
 }
